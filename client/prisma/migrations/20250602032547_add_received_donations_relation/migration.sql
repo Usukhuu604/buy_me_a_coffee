f17d95d5e68808f9fd52e8ed5fc88e99b6b20590
24LP0723 24LP0723 <24LP0723@ADMINs-iMac.local>
2025-06-02 12:33:50 +0800

    first commit

---
+++ client/prisma/migrations/20250602032547_add_received_donations_relation/migration.sql
@@ -0,0 +1,72 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "profileId" INTEGER NOT NULL,
+    "bankCard" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "avatarImage" TEXT NOT NULL,
+    "socialMediaURL" TEXT NOT NULL,
+    "backgroundImage" TEXT NOT NULL,
+    "successMessage" TEXT NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Donation" (
+    "id" SERIAL NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "specialMessage" TEXT NOT NULL,
+    "socialURLOrBuyMeACoffee" TEXT NOT NULL,
+    "donorId" INTEGER NOT NULL,
+    "recipientId" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Donation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bankCard" (
+    "id" SERIAL NOT NULL,
+    "country" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "cardNumber" TEXT NOT NULL,
+    "expiryDate" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "bankCard_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_profileId_key" ON "User"("profileId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_bankCard_key" ON "User"("bankCard");
+
+-- AddForeignKey
+ALTER TABLE "Donation" ADD CONSTRAINT "Donation_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
